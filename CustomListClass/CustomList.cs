cf2186ea4af812c85378c8e04a0595073a12304f
JoshAnnis <Josh_Annis@yahoo.com>
2017-02-02 18:15:05 -0600

    Got first test done

---
+++ CustomListClass/CustomList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+   public class CustomList<T> : IEnumerable
+    {
+
+    public T[] myArray = new T[1];
+        public int count = 0;
+        
+
+        public void Add(T result)
+        {
+
+            T[] tempArray = new T[myArray.Length + 5];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = tempArray[i];
+            }
+            count++;               
+            myArray = tempArray;
+        }
+
+        public void Remove(int vaule)
+        {
+
+        }
+    }
+}
---
+++ CustomListClass/CustomListTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClass;
+using System.Collections.Generic;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddtoArray()
+        {
+            //Arrange 
+            CustomList<int> list = new CustomList<int>();
+            int result = 1;
+            //Act
+            list.Add(45);
+            
+            //Assert
+            Assert.AreEqual(list.count, result);
+         }
+
+
+        [TestMethod]
+        public void RemoveformArray()
+        {
+            CustomList<int> list = new CustomList<int>();
+            int result = 0;
+            list.Remove(0);
+            Assert.AreEqual(list.myArray.Length, result);
+
+        }
+
+
+
+    }
+}
